10b96986d9c1f4b9c8cc0740f7f3eb19f1c8c0f3
Maheswara <113037933+mahesmaheswara@users.noreply.github.com>
2022-09-13 23:40:43 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " NAMA          : Anggara Maheswara" << endl;
+    cout << " NIM           : A11 2022 14594" << endl;
+    cout << " ALAMAT        : Wonosalam Demak" << endl;
+    cout << " Asal Sekolah  : SMA 1 Wiradesa Pekalongan" << endl;
+
+    return 0;
+}
